1985053adcd010ebb532dc07d3939fe01a20f230
sm <its.sayon.mitra@gmail.com>
2025-06-06 22:03:01 +0530

    accessing buttons, inputs using DOM

---
+++ assets/scripts/objects.js
@@ -0,0 +1,13 @@
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchButton = document.getElementById('search-btn');
+
+const movie = document.getElementById('title').value;
+const name = document.getElementById('extra-name').value;
+const value = document.getElementById('extra-value').value;
+
+const searchMovie = document.getElementById('filter-title').value;
+
+const display = document.getElementById('movie-list');
+
+addMovieButton.addEventListener('click');
+searchButton.addEventListener('click');
