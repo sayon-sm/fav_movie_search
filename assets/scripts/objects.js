f07d7ecf3f9ddb2f9abea476689ce9fbe9e84aee
sm <its.sayon.mitra@gmail.com>
2025-06-06 22:23:54 +0530

    adding inputs as objects in array

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,13 +1,25 @@
 const addMovieButton = document.getElementById('add-movie-btn');
 const searchButton = document.getElementById('search-btn');
 
-const movie = document.getElementById('title').value;
-const name = document.getElementById('extra-name').value;
-const value = document.getElementById('extra-value').value;
+const list = [];
 
 const searchMovie = document.getElementById('filter-title').value;
 
 const display = document.getElementById('movie-list');
 
-addMovieButton.addEventListener('click');
+function getInput() {
+  const movie = document.getElementById('title').value;
+  const name = document.getElementById('extra-name').value;
+  const value = document.getElementById('extra-value').value;
+
+  const input = {
+    name: movie,
+    title: name,
+    value: value,
+  };
+
+  list.push(input);
+}
+
+addMovieButton.addEventListener('click', getInput);
 searchButton.addEventListener('click');
